6d7d9040e5fa3ec45ba4d2546415b43851baabc7
Sara Vieira <hey@iamsaravieira.com>
2018-05-28 11:58:42 +0100

    n ew error

--- src/client.js
+++ src/client.js
@@ -2,16 +2,22 @@ import React from 'react'
 import { hydrate } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { ensureReady, After } from '@jaredpalmer/after'
-import ApolloClient, { gql } from 'apollo-boost'
+import gql from 'graphql-tag'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { withClientState } from 'apollo-link-state'
 import { ApolloProvider } from 'react-apollo'
 import routes from './routes'
-import * as OfflinePluginRuntime from 'offline-plugin/runtime'
 import { ThemeProvider } from 'styled-components'
 
 import Nav from './Components/Nav'
 import './Utils/global-styles'
 import theme from './Utils/theme'
 import { WATCHED_KEY, FAV_KEY, getStorage } from './Utils/state'
+
+const window = {}
 
 const defaultState = {
   favorites: getStorage(FAV_KEY),
@@ -20,8 +26,11 @@ const defaultState = {
   search: ''
 }
 
-const stateLink = {
+const cache = new InMemoryCache().restore(window.__APOLLO_STATE__)
+
+export const stateLink = withClientState({
   defaults: defaultState,
+  cache,
   resolvers: {
     Mutation: {
       addFavorite: (_, { id }, { cache }) => {
@@ -90,11 +99,16 @@ const stateLink = {
       }
     }
   }
-}
+})
 
 const client = new ApolloClient({
-  uri: 'https://api.graphcms.com/simple/v1/cjhdcwrb98if90109o4pzawaq',
-  clientState: stateLink
+  link: ApolloLink.from([
+    stateLink,
+    new HttpLink({
+      uri: 'https://api.graphcms.com/simple/v1/cjhdcwrb98if90109o4pzawaq'
+    })
+  ]),
+  cache
 })
 
 ensureReady(routes).then(data =>
@@ -121,5 +135,3 @@ ensureReady(routes).then(data =>
 if (module.hot) {
   module.hot.accept()
 }
-
-OfflinePluginRuntime.install()
